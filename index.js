fc889439d47d65961e837cd6b81f4d5d6ba1749b
James Ferguson <ncjamesferguson@gmail.com>
2018-06-03 01:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function createPost() {
+  
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  
 }
